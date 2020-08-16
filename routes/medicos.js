0127808edaf531adf36d4c9372810e5929dafdba
lucaseblock <lucasblock049@gmail.com>
2020-08-16 18:34:15 -0300

    Fin sección 13

--- routes/medicos.js
+++ routes/medicos.js
@@ -36,7 +36,11 @@ router.post(
 
 
 router.put('/:id',
-    [   
+    [
+        validarJWT,
+        check('nombre', 'El nombre del médico es necesario').not().isEmpty(),
+        check('hospital', 'El id del hospital debe ser válido').isMongoId(),
+        validarCampos   
     ],
     actualizarMedico
  );
