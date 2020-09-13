47b05a929cc968d500861b3d02877dc3a4358ea5
lucaseblock <lucasblock049@gmail.com>
2020-09-12 22:21:10 -0300

    SPA control

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
+const path = require('path');
 
 const express = require('express');
 const cors = require('cors');
 
-const { dbConnection } = require('./database/config')
+const { dbConnection } = require('./database/config');
+const { response } = require('express');
 
 //Crear el servidor express
 const app = express();
@@ -31,6 +33,12 @@ app.use( '/api/medicos', require('./routes/medicos') );
 app.use( '/api/todo', require('./routes/busquedas') );
 app.use( '/api/login', require('./routes/auth') );
 app.use( '/api/upload', require('./routes/uploads') );
+
+// lo útimo
+
+app.get('*', (req, res) =>{
+    res.sendFile( path.resolve( __dirname, 'public/index.html' ) );
+})
 
 app.listen( process.env.PORT, () => {
     console.log('Servidor corriendo en puerto ' + process.env.PORT);
